3b812420d6f3fc1c7168df2625295b8c406642aa
Isaac Williams <Isaaciyaye174@gmail.com>
2023-02-28 18:46:16 +0100

    Update ClientRow.tsx

--- client/src/components/ClientRow.tsx
+++ client/src/components/ClientRow.tsx
@@ -1,6 +1,7 @@
 import { FaTrash } from "react-icons/fa";
 import { useMutation } from "@apollo/client";
 import { DELETE_CLIENT } from "../mutations/clientMutations";
+import { GET_CLIENTS } from "../queries/clientQueries";
 interface Props {
   client: {
     name: string;
@@ -12,6 +13,18 @@ interface Props {
 const ClientRow = ({ client }: Props) => {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
+    // refetchQueries: [{ query: GET_CLIENTS }],
+    update(cache, { data: { deleteClient } }) {
+      const { clients }: any = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: {
+          clients: clients.filter(
+            (client: any) => client.id !== deleteClient.id
+          ),
+        },
+      });
+    },
   });
   return (
     <tr>
@@ -19,7 +32,9 @@ const ClientRow = ({ client }: Props) => {
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className="btn btn-danger btn-sm">
+        <button
+          className="btn btn-danger btn-sm"
+          onClick={() => deleteClient()}>
           <FaTrash />
         </button>
       </td>
